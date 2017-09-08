9f11ab859a24aafa4aa6b9d5690b23f9563118c4
OGIBAYASHI Hironori <ogibayashi@gmail.com>
2017-09-09 07:36:12 +0900

    fix

--- vars/helloMessage.groovy
+++ vars/helloMessage.groovy
@@ -1,9 +1,7 @@
-def person = [
-  'Alice': ['name': 'Alice', 'age': 10],
-              'Bob': ['name': 'Bob','age': 20 ]
-]
-
 def call(String name='human') {
+  def person = [ 'Alice': ['name': 'Alice', 'age': 10],
+                   'Bob': ['name': 'Bob','age': 20 ]
+                 ]
   echo "Hello, ${name}!, Your age is ${person['Alice']['age']}"
   echo "Hello, ${name}!"
 }
