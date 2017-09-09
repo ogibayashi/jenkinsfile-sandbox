34188b9b859290e7b39770e7614b365ea2723614
OGIBAYASHI Hironori <ogibayashi@gmail.com>
2017-09-10 07:36:33 +0900

    fix

--- vars/helloMessage.groovy
+++ vars/helloMessage.groovy
@@ -1,5 +1,5 @@
 def call(String name='human') {
-  def person = getConfig()
+  def person = getConfig(name)
   echo "Hello, ${name}!, Your age is ${person['Alice']['age']}"
   echo "Hello, ${name}!"
 }
