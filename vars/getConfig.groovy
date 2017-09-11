c3ed911289e43f82c84487b7260fea11460b50cb
OGIBAYASHI Hironori <ogibayashi@gmail.com>
2017-09-12 06:43:38 +0900

    fix

---
+++ vars/getConfig.groovy
@@ -0,0 +1,5 @@
+def getConfig(String name='Alice') {
+  [ 'Alice': ['name': 'Alice', 'age': 10],
+    'Bob': ['name': 'Bob','age': 20 ]
+  ]
+}
--- vars/helloMessage.groovy
+++ vars/helloMessage.groovy
@@ -1,8 +1,8 @@
-def getConfig(String name='Alice') {
-  [ 'Alice': ['name': 'Alice', 'age': 10],
-    'Bob': ['name': 'Bob','age': 20 ]
-  ]
-}
+// def getConfig(String name='Alice') {
+//   [ 'Alice': ['name': 'Alice', 'age': 10],
+//     'Bob': ['name': 'Bob','age': 20 ]
+//   ]
+// }
 
 def call(String name='human') {
   def person = getConfig(name)
